1f4aa8f25f09040e1ece44fe7584100397831304
Alexander Wermuth <alexwer2@gmail.com>
2022-07-30 13:49:41 +0200

    Added hot reload to docker

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 8000;
+  const configService = app.get(ConfigService);
+  const port = +configService.get<string>('SERVER_PORT');
   app.enableCors({
     origin: ['http://localhost:3000'],
   });
